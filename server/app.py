ca5c5366b1784981521752025acaec8c5c7fb088
Nick Mugo <nicknjuru13@gmail.com>
2023-12-02 16:01:47 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,45 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
-
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if request.method == 'PATCH':
+        for attribute in request.json:
+            setattr(message, attribute, request.json.get(attribute))
+        db.session.commit()
+        message_to_dict = message.to_dict() 
+        response = jsonify(message_to_dict)
+        response_body = make_response(response, 200)
+        response_body.headers["Content-Type"] = "application/json"
+        return response_body
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {'message': 'Delete was successful'}
+        response = make_response(jsonify(response_body), 200)
+        response.headers["Content-Type"] = "application/json"
+        return response
+
+@app.route('/messages', methods=['GET', 'POST'])
+def get_messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_list = [{'id': msg.id, 'body': msg.body, 'created_at': msg.created_at, 'updated_at': msg.updated_at} for msg in messages]
+        response = jsonify(messages_list)
+        return make_response(response, 200)
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.json.get("body"),
+            username=request.json.get("username"),
+            created_at=request.json.get("created_at"),
+            updated_at=request.json.get("updated_at")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        new_message_to_dict = new_message.to_dict()
+        response = jsonify(new_message_to_dict)
+        return make_response(response, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
